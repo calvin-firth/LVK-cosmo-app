110672fc13f602423730be6e20615497f8c917db
Calvin <firth016@umn.edu>
2025-07-23 11:43:09 +0200

    Formatting

--- streamlit_app/Notices test.py
+++ streamlit_app/Notices test.py
@@ -44,7 +44,7 @@ for event in st.session_state["redis"].smembers("events:all"):
     # 🔤 Show plain UTF-8 decoded fields
     if decoded_data:
         #st.subheader("Event Info")
-        styler = (pd.DataFrame.from_dict(decoded_data)).style.hide_index().format(subset=['mean'], decimal=',', precision=2)
+        styler = (pd.DataFrame.from_dict(decoded_data)).style.hide().format(subset=['mean'], decimal=',', precision=2)
         st.write(styler.to_html(), unsafe_allow_html=True)
 
     # 📝 Show JSON fields
