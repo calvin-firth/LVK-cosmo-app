f5e5e7b244db709a04af92888b2073bf924d6334
Calvin <firth016@umn.edu>
2026-02-15 11:10:46 -0600

    Put new database link

--- streamlit_app/All events.py
+++ streamlit_app/All events.py
@@ -6,7 +6,7 @@ import io
 
 if "redis2" not in st.session_state:
     st.session_state["redis2"] = redis.Redis.from_url(
-        "rediss://default:AWTjAAIjcDE0ODhlMDIxZTEwNDg0Y2NmOTM5YTliZWI4ZTE0OGI5ZHAxMA@internal-sawfly-25827.upstash.io:6379",
+        "rediss://default_ro:AuEiAAIgcDIuQ6LLfLtc9kt4C1IgcAba7p2sLT-NK6bZjTFpNoICyQ@merry-grackle-57634.upstash.io:6379",
         decode_responses=False,retry_on_timeout=True)
 st.write("Use the dropdown menu to view the analysis results for all previously analyzed events (currently only includes LVK's O4 operating run)")
 selected_event = st.selectbox("", ["Choose an event..."] + st.session_state["events"],label_visibility="collapsed")
