0a813ccc65160e4f4ec5d754771e8f678cc769c1
Calvin <firth016@umn.edu>
2026-03-16 19:05:54 -0600

    change all events page

--- streamlit_app/All events.py
+++ streamlit_app/All events.py
@@ -19,6 +19,7 @@ if "Event table" not in st.session_state:
     ).apply(pd.to_numeric, errors="ignore")
     st.session_state["Event table"]=tbl_df
     st.session_state["Event table"].columns = ["50% Sky-localization area", "90% Sky-localization area", "Luminosity distance (Mpc)"]
+    st.session_state["Event table"].reset_index().rename(columns={'index': "Event"})
     for event in st.session_state["events"]:
         if event not in st.session_state["Event table"].index:
             st.write(event)
@@ -47,10 +48,10 @@ if "Event table" not in st.session_state:
             # Show plain UTF-8 decoded fields
             if decoded_data:
                 st.write(decoded_data)
-                st.session_state["Event table"][event] = decoded_data.values()[:-1]
+                st.session_state["Event table"].loc[event] = decoded_data.values()[:-1]
 
     st.session_state["Event table"]["is_checked"] = np.zeros(len(st.session_state["Event table"]["50% Sky-localization area"]), dtype=bool)
-    st.session_state["Event table"].reset_index().rename(columns={'index':"Event"})
+
     posteriors = []
     retracted = 0
     num_post = 0
