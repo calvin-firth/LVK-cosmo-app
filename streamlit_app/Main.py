4e8c9e9f6a6682c958bd50024aa8b3dc9b520f16
Calvin <firth016@umn.edu>
2026-03-16 22:11:56 -0600

    change all events page

--- streamlit_app/Main.py
+++ streamlit_app/Main.py
@@ -29,6 +29,55 @@ except:
     st.session_state["queued"] = "queue too large to read"
 st.session_state["events"]=sorted(st.session_state["redis"].smembers("events:all"),reverse=True)
 
+if "Event table" not in st.session_state:
+    tbl_json = st.session_state["redis2"].json().get("event_list_metadata",'$')[0]
+    tbl_df = pd.read_json(StringIO(tbl_json),dtype=float)
+    tbl_df = tbl_df.applymap(
+        lambda x: x[0] if isinstance(x, list) else x
+    ).apply(pd.to_numeric, errors="ignore")
+    st.session_state["Event table"]=tbl_df
+    st.session_state["Event table"].columns = ["50% Sky-localization area", "90% Sky-localization area", "Luminosity distance (Mpc)"]
+    for event in st.session_state["events"]:
+        if event not in st.session_state["Event table"].index:
+            decoded_data = {}
+            binary_data = {}
+            json_data = {}
+
+            raw_data = st.session_state["redis2"].hgetall(event)
+            raw_data = dict(sorted(raw_data.items()))
+
+            for k, v in raw_data.items():
+                key = k.decode() if isinstance(k, bytes) else k
+                try:
+                    value = v.decode("utf-8")
+
+                    # Check if value is valid JSON
+                    try:
+                        parsed_json = pd.read_json(StringIO(value))
+                        json_data[key] = parsed_json
+                    except:
+                        decoded_data[key] = [value]
+
+                except UnicodeDecodeError:
+                    binary_data[key] = v  # Leave binary
+
+            # Show plain UTF-8 decoded fields
+            if decoded_data:
+                mapper = {'50% area': '{0:,.2f}',
+                          '90% area': '{0:,.2f}',
+                          'dl': '{0:,.2f}'
+                          }
+                df = pd.DataFrame.from_dict(decoded_data)
+                for col in ['50% area', '90% area', 'dl']:
+                    df[col] = pd.to_numeric(df[col], errors='coerce')
+                st.session_state["Event table"] = pd.DataFrame(np.concatenate([st.session_state["Event table"].values,df.values]),columns=st.session_state["Event table"].columns)
+    st.session_state["Event table"].reset_index().rename(columns={'index': "Event"})
+    st.session_state["Event table"]["is_checked"] = np.zeros(len(st.session_state["Event table"]["50% Sky-localization area"]), dtype=bool)
+
+    posteriors = []
+    retracted = 0
+    num_post = 0
+
 st.session_state["status"]["Connected"] = ((time.time() - float(st.session_state["status"]["Last Check"])) < 60)
 
 tz_offset = st_javascript("new Date().getTimezoneOffset();")
